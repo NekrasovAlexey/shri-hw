8229dfb8a4ccd56e8c0c0562b482451bfdaff076
Nekrasov Alexey <avnekr@yandex.ru>
2018-10-13 18:11:31 +0300

    ДЗ - «Node js» Добавил сервер на express Получение времени работы сервера. Получение списка событий с фильтром по типу POST-запросом 404 страницу для остальных запросов.

---
+++ server/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const config = require('./config');
+const errorCatcher = require('./middleware/errorCatcher');
+const setCustomData = require('./middleware/setCustomData');
+const setRoutes = require('./Routes').setRoutes;
+
+const data = {};
+
+const app = express();
+
+/**
+ * Парсим переданные json данные.
+ */
+app.use(bodyParser.json());
+
+/**
+ * Сохраняем время старта сервера.
+ */
+app.use(setCustomData(data));
+
+/**
+ * Задаем роуты.
+ */
+setRoutes(app);
+
+/**
+ * Перехватываем ошибки.
+ */
+app.use(errorCatcher);
+
+const port = process.argv[2] ? Number(process.argv[2]) : config.port;
+
+app.listen(port, error => {
+    if (error) {
+        return console.log('Произошла ошибка', error);
+    }
+
+    data.startDate = new Date();
+    console.log(`Сервер запущен на ${port}`);
+});
